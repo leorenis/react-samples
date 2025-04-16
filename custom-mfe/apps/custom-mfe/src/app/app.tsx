f6dcdf065dd8b81cc8b0d6202d3b33146c085e15
leorenis <leorenis@gmail.com>
2025-04-15 21:10:39 -0300

    nx: learning. Add Module Federation

--- custom-mfe/apps/custom-mfe/src/app/app.tsx
+++ custom-mfe/apps/custom-mfe/src/app/app.tsx
@@ -1,11 +1,13 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+//@ts-expect-error
+import RemoteButton from  'products/RemoteButton'
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="custom-mfe" />
+      <p>
+        Hi, I'am the HOST.
+      </p>
+      <RemoteButton />
     </div>
   );
 }
