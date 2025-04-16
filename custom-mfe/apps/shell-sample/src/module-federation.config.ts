f6b5a03cb1fecb4a448db8d9517aaace73783c2e
leorenis <leorenis@gmail.com>
2025-04-15 22:34:38 -0300

    nx: learning. chage from remoteEntry to mf-manifest.json

--- custom-mfe/apps/shell-sample/src/module-federation.config.ts
+++ custom-mfe/apps/shell-sample/src/module-federation.config.ts
@@ -2,7 +2,7 @@ import { ModuleFederationConfig } from '@nx/module-federation';
 
 const config: ModuleFederationConfig = {
   name: 'shell-sample',
-  remotes: [['products', 'products@http://localhost:4201/remoteEntry.js']],
+  remotes: [['products', 'products@http://localhost:4201/mf-manifest.json']],
 };
 
 export default config;
