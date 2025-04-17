a50e01f7a32f18e81d81130046e833753f88a05f
leorenis <leorenis@gmail.com>
2025-04-16 22:06:20 -0300

    learn: setup css module in a project

--- custom-mfe/apps/products/src/components/remote-button.tsx
+++ custom-mfe/apps/products/src/components/remote-button.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import styles from './remote-button.module.css';
 export default function Button() {
 
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className={styles['button']}>
       <p>{count}</p>
       <button type="button" onClick={() => setCount(prev => prev +=1)}>Remote button</button>
     </div>
--- custom-mfe/apps/products/src/module-federation.config.ts
+++ custom-mfe/apps/products/src/module-federation.config.ts
@@ -10,6 +10,7 @@ const config: ModuleFederationConfig = {
   name: 'products',
   exposes: {
     './RemoteButton': './src/components/remote-button.tsx',
+    './Types': './src/types.d.ts',
   },
   shared: (libraryName, sharedConfig) => {
     if (!sharedLibraries.includes(libraryName)) {
