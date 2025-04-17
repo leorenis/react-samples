aecf4522cf8602dae8bc8116ab45579ae10d1b64
leorenis <leorenis@gmail.com>
2025-04-16 22:15:22 -0300

    learn: change module federation from shell-sample

--- custom-mfe/apps/shell-sample/src/module-federation.config.ts
+++ custom-mfe/apps/shell-sample/src/module-federation.config.ts
@@ -1,8 +1,18 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
 
+const sharedLibraries = [
+  "react",
+  "react-dom",
+];
+
 const config: ModuleFederationConfig = {
   name: 'shell-sample',
   remotes: [['products', 'products@http://localhost:4201/mf-manifest.json']],
+  shared: (libraryName, sharedConfig) => {
+    if (!sharedLibraries.includes(libraryName)) {
+      return false;
+    }
+  }
 };
 
 export default config;
