75cce9a184eb72650aeac246b75ae8680b48082b
leorenis <leorenis@gmail.com>
2025-05-09 20:27:23 -0300

    [test]  @vitest tests, adding prettier

--- udmy-webpack/5-libs/src/libs/index.js
+++ udmy-webpack/5-libs/src/libs/index.js
@@ -1,4 +1,4 @@
-import { isCNPJ } from './isCNPJ/index.mjs'
-import { isCPF } from './isCPF/index.mjs'
+import { isCNPJ } from './isCNPJ/index.mjs';
+import { isCPF } from './isCPF/index.mjs';
 
-export { isCNPJ, isCPF }
+export { isCNPJ, isCPF };
