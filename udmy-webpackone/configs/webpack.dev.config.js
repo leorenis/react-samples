db2f14cdcf488cfdc5e6f9dc71d41c7f75369127
leorenis <leorenis@gmail.com>
2025-05-08 21:54:31 -0300

    [clean]  sample strucuture

--- udmy-webpackone/configs/webpack.dev.config.js
+++
@@ -1,7 +0,0 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.config');
-const devConfig = {
-  mode: 'development'
-};
-
-module.exports = merge(commonConfig, devConfig);
