2e66280fac3f9bf523d5b2edabf723772de41bb9
leorenis <leorenis@gmail.com>
2025-05-09 20:12:25 -0300

    [test]  @vitest tests, adding prettier

--- udmy-webpack/5-libs/src/libs/isCPF/index.test.mjs
+++ udmy-webpack/5-libs/src/libs/isCPF/index.test.mjs
@@ -1,9 +1,8 @@
-import { describe, it, expect } from 'vitest'
-import { isCPF } from '../isCPF/index.mjs'
+import { describe, it, expect } from 'vitest';
+import { isCPF } from '../isCPF/index.mjs';
 
 describe('isCPF Test case', () => {
-
   it.each([null, undefined, ''])('CPF should be invalid: %s', (cpf) => {
-    expect(isCPF(cpf)).toBeFalsy()
+    expect(isCPF(cpf)).toBeFalsy();
   });
 });
