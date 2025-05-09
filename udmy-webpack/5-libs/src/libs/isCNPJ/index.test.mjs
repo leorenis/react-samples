50fc829e6ec294423a25ad1e731b1066ded12288
leorenis <leorenis@gmail.com>
2025-05-08 22:33:32 -0300

    [test]  @vitest configs

---
+++ udmy-webpack/5-libs/src/libs/isCNPJ/index.test.mjs
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest'
+import { isCNPJ } from './index.mjs'
+
+describe('isCNPJ Test case', () => {
+
+  it.each([null, undefined, '', ' '])(
+    "CNPJ should NOT be '%s'",
+    (cnpj) => expect(isCNPJ(cnpj)).toBe(false)
+  )
+  it('CNPJ should be valid', () => {
+    expect(isCNPJ('11.222.333/0001-81')).toBe(true)
+  });
+
+  it.each(['11.222.333/0001', '11.222.333'])(
+    'CNPJ have to lenght 14 chars: %s',
+    (cnpj) => {
+      expect(isCNPJ(cnpj)).toBe(false)
+    }
+  )
+
+  it.each(['11.222.333/0001-91', '11.222.333/0001-82'])(
+    'CNPJ should have RIGHT digit calculation: %s',
+    (cnpj) => {
+      expect(isCNPJ(cnpj)).toBe(false)
+    }
+  )
+});
