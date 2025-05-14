624f667c52fe35df2de1eb5eb9c4009f6ea1982a
leorenis <leorenis@gmail.com>
2025-05-13 21:30:50 -0300

    refactory: configs ESLint

--- udmy-webpack/5-libs/eslint.config.js
+++ udmy-webpack/5-libs/eslint.config.js
@@ -1,14 +1,26 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 
+// const rules = pluginJs.configs.recommended;
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    languageOptions: { 
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
       globals: {
-        ...globals.browser, ...globals.node
-      }
-    } 
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      // bypass
+    },
+  },
+  {
+    ignores: ['node_modules', 'coverage', 'dist', 'eslint.config.mjs'],
   },
 ]);
