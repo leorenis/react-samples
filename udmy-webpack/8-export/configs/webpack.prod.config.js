d287f02889fbe595d6617a77ce93b573a73f947e
leorenis <leorenis@gmail.com>
2025-05-17 18:53:05 -0300

    refactory: tsconfig prod

--- udmy-webpack/8-export/configs/webpack.prod.config.js
+++ udmy-webpack/8-export/configs/webpack.prod.config.js
@@ -3,6 +3,22 @@ import common from './webpack.common.config.js'
 
 const configProd = merge(common, {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // regex: x? indicate that x? is optional.
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.prod.json',
+            }
+          }
+        ],
+      }
+    ]
+  }
 })
 
 export default configProd
