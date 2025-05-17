8920fe796472cab6e713f87cafecf3d3008bec40
leorenis <leorenis@gmail.com>
2025-05-17 11:07:24 -0300

    config: vitest

--- udmy-webpack/7-ts/vite.config.js
+++ udmy-webpack/7-ts/vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.mjs', 'src/**/*.test.js'],
+    include: ['src/**/*.test.ts', 'src/**/*.test.js'],
     coverage: {
       provider: 'v8',
       include: [
-        'src/libs/**/*.mjs',
+        'src/libs/**/*.ts',
         'src/libs/**/*.js',
       ],
       exclude: [
-        'src/**/*.test.mjs',
-        'src/libs/index.mjs',
+        'src/**/*.test.ts',
+        'src/libs/index.ts',
+        'src/main.ts',
         'src/**/*.test.js',
         'src/libs/index.js',
       ],
