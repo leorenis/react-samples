dd5368112750825a167d310c43012ca4f102be1d
leorenis <leorenis@gmail.com>
2025-05-27 20:44:30 -0300

    learn: webpack react sample

--- udmy-webpack/9-import-export/9-export-cpf/src/app.js
+++ udmy-webpack/9-import-export/9-export-cpf/src/app.js
@@ -1,12 +1,35 @@
-import { StrictMode } from 'react'
+import { StrictMode, use, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const CpfInput = (props) => {
+  const { id, name, label, value, onChange } = props
+  const [cpf, setCpf] = useState(value)
+
+  handleChange = (event) => {
+    setCpf(event.target.value)
+    onChange(cpf)
+  }
+
+  return (
+    <input 
+      key={id} 
+      id={id} 
+      name={name}
+      type='text'
+      value={cpf}
+      placeholder={label}
+      onChange={(e) => handleChange(e)}
+    /> 
+  )
+}
 
 root.render(
   <StrictMode>
     <div>
       Webpack component
     </div>
+    
   </StrictMode>
 )
