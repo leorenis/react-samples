1685ab1c9af50ca1c3630f35832d76579639620c
leorenis <leorenis@gmail.com>
2025-06-10 20:24:00 -0300

    app: tailwind

--- udmy-webpack/10-module-federation/10A-auth/src/app.js
+++ udmy-webpack/10-module-federation/10A-auth/src/app.js
@@ -15,7 +15,7 @@ const mount = (element) => {
 
 // Performs only development mode
 if (process.env.NODE_ENV === DEV_MODE) {
-  const root = document.getElementById('root')
+  const root = document.getElementById('sandbox-auth-root')
   mount(root)
 }
 
