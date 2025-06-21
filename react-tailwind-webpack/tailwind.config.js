32656d985e18716c099eb89688b9448ecf3b4872
leorenis <leorenis@gmail.com>
2025-06-21 12:58:50 -0300

    feat: webpack tailwind postcss plugin

---
+++ react-tailwind-webpack/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Isso é o "purge"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
---
+++ react-tailwind-webpack/webpack.config.dev.js
@@ -0,0 +1,47 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = path.resolve();
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    port: 3011,
+    historyApiFallback: true, // necessário para React Router
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      scriptLoading: 'module',
+    })
+  ]
+}
+
+export default config
