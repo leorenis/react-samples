72da97690c198cf6d0f7ee8428cca447e5c0d18b
leorenis <leorenis@gmail.com>
2025-06-21 13:26:34 -0300

    feat: training

--- react-tailwind-webpack/src/components/sandbox/Sandbox.js
+++ react-tailwind-webpack/src/components/sandbox/Sandbox.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Sandbox = () => {
   return (
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-      <div class="bg-white p-6 rounded shadow-md text-center">
-        <h1 class="text-2xl font-bold text-blue-600">Olá, Tailwind!</h1>
-        <p class="mt-2 text-gray-600">Você começou com sucesso.</p>
+    <div class='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div class='bg-white p-6 rounded shadow-md text-center'>
+        <h1 class='text-2xl font-bold text-blue-600'>Olá, Tailwind!</h1>
+        <p class='mt-2 text-gray-600'>Você começou com sucesso.</p>
       </div>
     </div>
   );
