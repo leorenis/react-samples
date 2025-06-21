6a673f4bfaf87323e83296e2459f784caf5746e1
leorenis <leorenis@gmail.com>
2025-06-21 13:08:45 -0300

    feat: sandbox test component

---
+++ react-tailwind-webpack/src/components/sandbox/Sandbox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sandbox = () => {
+  return (
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+      <div class="bg-white p-6 rounded shadow-md text-center">
+        <h1 class="text-2xl font-bold text-blue-600">Olá, Tailwind!</h1>
+        <p class="mt-2 text-gray-600">Você começou com sucesso.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sandbox;
