dc7084abf71ce590a0816fb7488d5c101d8d7396
leorenis <leorenis@gmail.com>
2025-06-30 20:59:06 -0300

    tailwind: learning responsive breakpoints

--- react-tailwind-webpack/src/components/breakpoints/Responsive.js
+++ react-tailwind-webpack/src/components/breakpoints/Responsive.js
@@ -5,7 +5,7 @@ const Responsive = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className='bg-white p-6 rounded shadow-md text-center w-80 md:w-[30rem]'>
         <h1 className='text-xl md:text-2xl lg:text-4xl font-bold text-blue-600'>Hello responsive</h1>
-        <p className='mt-4 text-sm md:text-base lg:text-lg text-gray-600'>The font and container changes conform the screen size</p>
+        
       </div>
     </div>
   )
