ed0362afe6a18b6a508f9fdc6e406b4dd20df363
leorenis <leorenis@gmail.com>
2025-07-05 20:57:32 -0300

    tailwind: samples

--- react-tailwind-webpack/src/components/sandbox/Sandbox.js
+++ react-tailwind-webpack/src/components/sandbox/Sandbox.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Sandbox = () => {
   return (
-    <div class='flex items-center justify-center min-h-screen bg-gray-100'>
+    <div class='flex items-center justify-center min-h-screen bg-blue-100'>
       <div class='bg-white p-6 rounded shadow-md text-center'>
-        <h1 class='text-2xl font-bold text-blue-600'>Hello, Tailwind!</h1>
+        <h1 class='text-2xl font-bold text-gray-600'>Hello, Tailwind!</h1>
         <p class='mt-2 text-gray-600'>We have started successfully.</p>
       </div>
     </div>
