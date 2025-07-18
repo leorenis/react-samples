1f7a46f2f1ed40b4c741805851ee1927c4112231
leorenis <leorenis@gmail.com>
2025-07-17 22:20:10 -0300

    fix: box

--- react-tailwind-webpack/src/components/boxes/Boxes.js
+++ react-tailwind-webpack/src/components/boxes/Boxes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Sandbox = () => {
   return (
-    <div className='min-h-screen bg-gray-200 flex items-center justify-center'>
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
       <div className='bg-white p-6 rounded shadow-md flex flex-col gap-4'>
         <div className='bg-green-500 text-white p-4 rounded w-32 text-center'>Box two (1)</div>
         <div className='bg-yellow-400 text-white p-4 rounded w-32 text-center'>Box one (2)</div>
