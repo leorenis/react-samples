b35e2761bea85fdc2bb26fa2a7e5a2e4ab330d70
leorenis <leorenis@gmail.com>
2025-07-24 21:37:26 -0300

    fix: order

--- react-tailwind-webpack/src/components/boxes/Boxes.js
+++ react-tailwind-webpack/src/components/boxes/Boxes.js
@@ -4,8 +4,8 @@ const Sandbox = () => {
   return (
     <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
       <div className='bg-white p-6 rounded shadow-md flex flex-col gap-4'>
-        <div className='bg-green-500 text-white p-4 rounded w-32 text-center'>Box two (1)</div>
-        <div className='bg-yellow-400 text-white p-4 rounded w-32 text-center'>Box one (2)</div>
+        <div className='bg-green-500 text-white p-4 rounded w-32 text-center'>Box one (1)</div>
+        <div className='bg-yellow-400 text-white p-4 rounded w-32 text-center'>Box two (2)</div>
         <div className='bg-red-500 text-white p-4 rounded w-32 text-center'>Box three (3)</div>
       </div>
     </div>
