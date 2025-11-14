754d790c7b1da550e216dfa52635d61093eee076
leorenis <leorenis@gmail.com>
2025-11-13 22:33:01 -0300

    learn: Link component

--- react-typescript/luz-e-cena/src/App.tsx
+++ react-typescript/luz-e-cena/src/App.tsx
@@ -1,9 +1,15 @@
 import Banner from "./components/Banner"
+import Link from "./components/Link"
 
 function App() {
 
   return (
+  <>
+    <Link href="/">
+      Link
+    </Link>
     <Banner src='./banner.png' alt='Onde a realidade encontra a fantasia!' />
+  </>
   )
 }
 
---
+++ react-typescript/luz-e-cena/src/components/Link/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Link.module.css'
+
+const Link = ({ children, ...othersProps }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
+  <a className={styles.link} {...othersProps}>
+    { children }
+  </a>
+)
+
+export default Link
