04c58db7bd7a96ff528faa63fb60567285951e71
leorenis <leorenis@gmail.com>
2025-11-19 20:37:15 -0300

    learn: Button component: classnames

--- react-typescript/luz-e-cena/src/components/Button/index.tsx
+++ react-typescript/luz-e-cena/src/components/Button/index.tsx
@@ -1,12 +1,23 @@
+import classNames from 'classnames'
+import styles from './Button.module.css'
 
 type ButtonProps = {
-  variant: 'default' | 'icon'
+  variant: 'default' | 'icon' // union
 } & React.ButtonHTMLAttributes<HTMLButtonElement> // & define elementos intricecos.
 
-const Button = ({ children }: ButtonProps) => (
-  <button>
-    { children }
-  </button>
-)
+const Button = ({ children, variant = 'default', ...othersProps }: ButtonProps) => {
+  const classMap = {
+    default: styles.default,
+    icon: styles.icon
+  }
+  return (
+    <button
+      className={classNames(styles.botao, classMap[variant])}
+      {...othersProps}
+    >
+      { children }
+    </button>
+  )
+}
 
 export default Button
