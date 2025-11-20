c9e19254a6399e72a8f53dcf69bf63d1eca657e2
leorenis <leorenis@gmail.com>
2025-11-19 21:59:01 -0300

    learn: MenuList

---
+++ react-typescript/luz-e-cena/src/components/Menu/MenuList/index.tsx
@@ -0,0 +1,10 @@
+import styles from './MenuList.module.css'
+
+const MenuList = ({ children }: React.HTMLAttributes<HTMLUListElement> ) => (
+  <nav>
+    <ul className={styles.navegacao}>
+      { children }
+    </ul>
+  </nav>
+)
+export default MenuList
