d2edb504eb685c4bb8f184041d0150d119849fb7
leorenis <leorenis@gmail.com>
2026-01-06 21:56:40 -0300

    new: Header

--- react-typescript/luz-e-cena/src/components/Header/index.tsx
+++ react-typescript/luz-e-cena/src/components/Header/index.tsx
@@ -1,5 +1,26 @@
+import Logo from '../Logo'
+import HeaderActions from './components/HeaderActions'
+import HeaderFormFilters from './components/HeaderFormFilters'
+import HeaderLinks from './components/HeaderLinks'
+import HeaderList from './components/HeaderList'
+import HeaderListItem from './components/HeaderListItem'
 
 const Header = () => (
-  <>Header</>
+  <header>
+    <HeaderList>
+      <HeaderListItem>
+        <Logo src='/logo.png' />
+      </HeaderListItem>
+      <HeaderListItem>
+        <HeaderLinks />
+      </HeaderListItem>
+      <HeaderListItem>
+        <HeaderFormFilters />
+      </HeaderListItem>
+      <HeaderListItem>
+        <HeaderActions />
+      </HeaderListItem>
+    </HeaderList>
+  </header>
 )
 export default Header
