5f883ff35c7abc710b1c5f19838ac3dbf2987f6a
leorenis <leorenis@gmail.com>
2026-01-12 21:27:50 -0300

    new

--- react-typescript/luz-e-cena/src/components/Link/index.tsx
+++ react-typescript/luz-e-cena/src/components/Link/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Link.module.css'
 
+
 const Link = ({ children, ...othersProps }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
   <a className={styles.link} {...othersProps}>
     { children }
