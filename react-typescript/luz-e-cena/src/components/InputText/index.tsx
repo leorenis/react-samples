7b35fb4063e62e0f7716fca60f491447ce5118d8
leorenis <leorenis@gmail.com>
2026-01-14 07:59:11 -0300

    inputtext

---
+++ react-typescript/luz-e-cena/src/components/InputText/index.tsx
@@ -0,0 +1,9 @@
+import styles from './InputText.module.css'
+
+const InputText = ({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <input className={styles.input} {...props} />
+  );
+};
+
+export default InputText
