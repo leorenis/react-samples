660f9fce1ec14eb1a983d0aaf2009b2727d99b13
leorenis <leorenis@gmail.com>
2026-02-03 08:51:53 -0300

    learn: movie section

--- react-typescript/luz-e-cena/src/types/index.ts
+++ react-typescript/luz-e-cena/src/types/index.ts
@@ -2,7 +2,7 @@ export type Categoria = '2D' | '3D'
 export type Censura = 'Livre' | '10 anos' | '12 anos' | '14 anos' | '16 anos'
 
 export interface ICardMovieProps {
-  id: string;
+  id: string | number;
   alt: string;
   src: string;
   titulo: string;
