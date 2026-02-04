00b8e248881cc801a179c9cbd09e2e391aa51977
leorenis <leorenis@gmail.com>
2026-02-04 07:21:42 -0300

    learn: newsletter component...

--- react-typescript/luz-e-cena/src/App.tsx
+++ react-typescript/luz-e-cena/src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner"
 import Header from "./components/Header"
 import MovieSection from "./components/MovieSection"
+import Newsletter from "./components/Newsletter"
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <Header />
     <Banner src='./banner.png' alt='Onde a realidade encontra a fantasia!' />
     <MovieSection />
+    <Banner src="./combo.png" alt="Combo" />
+    <Newsletter />
   </>
   )
 }
